582549473c0c8b0949db8a15fe91a8770654a19e
Iqbal <iqbal.a0909@gmail.com>
2021-11-16 17:46:56 +0800

    corrected the version text file

--- forceplateLoadCellFirmware.ino
+++ forceplateLoadCellFirmware.ino
@@ -4,8 +4,8 @@
 #include <WiFiClientSecure.h>
 #include "cert.h"
 
-const char * ssid = "home_wifi";
-const char * password = "helloworld";
+const char * ssid = "MinD@unifi";
+const char * password = "mind1234";
 
 
 String FirmwareVer = {
